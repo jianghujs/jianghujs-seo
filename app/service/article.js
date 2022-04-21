3b47e3ee8f3e63b72b62efc9cb0ded8524ac1cd9
Liu Ji <mynameisliuji@foxmail.com>
2022-04-21 22:22:02 +0800

    article

--- app/service/article.js
+++ app/service/article.js
@@ -1,6 +1,6 @@
 'use strict';
 const Service = require('egg').Service;
-const { tableEnum, articlePublishStatusEnum } = require("../constant/constant");
+const { tableEnum } = require("../constant/constant");
 const _ = require("lodash");
 const path = require("path");
 
@@ -10,131 +10,11 @@ const utc = require('dayjs/plugin/utc')
 const timezone = require('dayjs/plugin/timezone') // dependent on utc plugin
 dayjs.extend(utc)
 dayjs.extend(timezone)
-
-const idGenerateUtil = require("egg-jianghu/app/common/idGenerateUtil");
-const validateUtil = require("egg-jianghu/app/common/validateUtil");
 const { BizError, errorInfoEnum } = require("../constant/error");
-const fs = require("fs"),
-    fsPromises = require("fs").promises,
-    unlink = fsPromises.unlink,
-    rename = fsPromises.rename,
-    util = require("util"),
-    exists = util.promisify(fs.exists);
 const actionDataScheme = Object.freeze({
-  deletedArticle: {
-    type: "object",
-    additionalProperties: true,
-    required: ["articleId"],
-    properties: {
-      articleId: { anyOf: [{ type: "string" }, { type: "number" }] },
-    },
-  },
-  restoreArticle: {
-    type: "object",
-    additionalProperties: true,
-    required: ["articleId"],
-    properties: {
-      articleId: { anyOf: [{ type: "string" }, { type: "number" }] },
-    },
-  },
 });
 
 class ArticleService extends Service {
-
-  async fillInsertItemParamsBeforeHook() {
-    const { userId, username } = this.ctx.userInfo;
-    const time = dayjs().format();
-    const tableName = "article";
-    const columnName = "articleId";
-    const articleId = await idGenerateUtil.idPlus({
-      knex: this.app.jianghuKnex,
-      tableName,
-      columnName,
-    });
-    // 浏览器传过来的时间 要转成 Asia/Shanghai；
-    let { articlePublishTime } = this.ctx.request.body.appData.actionData;
-    if (articlePublishTime) {
-      articlePublishTime = dayjs(articlePublishTime).tz("Asia/Shanghai").format();
-    }
-    if (!dayjs(articlePublishTime).isValid()) {
-      articlePublishTime = dayjs().tz("Asia/Shanghai").format();
-    }    
-    Object.assign(this.ctx.request.body.appData.actionData, {
-      articleId,
-      articlePublishTime,
-      articleCreateTime: time,
-      articleCreateUserId: userId,
-      articleCreateUsername: username,
-      articleUpdateTime: time,
-      articleUpdateUserId: userId,
-      articleUpdateUsername: username,
-    })
-  }
-
-  async fillUpdateItemParamsBeforeHook() {
-    const { userId, username } = this.ctx.userInfo;
-    const time = dayjs().format();
-    // 浏览器传过来的时间 要转成 Asia/Shanghai；
-    let { articlePublishTime } = this.ctx.request.body.appData.actionData;
-    if (articlePublishTime) {
-      articlePublishTime = dayjs(articlePublishTime).tz("Asia/Shanghai").format();
-    }
-    if (!dayjs(articlePublishTime).isValid()) {
-      articlePublishTime = dayjs().tz("Asia/Shanghai").format();
-    }
-    Object.assign(this.ctx.request.body.appData.actionData, {
-      articlePublishTime,
-      articleUpdateTime: time,
-      articleUpdateUserId: userId,
-      articleUpdateUsername: username,
-    })
-  }
-
-  async articleHistoryRecordAfterHook() {
-    const { articleId } = this.ctx.request.body.appData.actionData;
-    if (articleId) {
-      // 保存新版本
-      const {id, ...history} = this.ctx.request.body.appData.actionData;
-      await this.app.jianghuKnex(tableEnum.article_history).insert(history);
-    }
-  }
-
-  async deletedArticle() {
-    const { jianghuKnex } = this.app;
-    const actionData = this.ctx.request.body.appData.actionData;
-    validateUtil.validate(actionDataScheme.deletedArticle, actionData);
-    const { articleId } = actionData;
-    const { articleMaterialDir } = this.app.config;
-
-    await jianghuKnex(tableEnum.article, this.ctx).where({ articleId }).update({ articlePublishStatus: articlePublishStatusEnum.deleted });
-
-    const articleDirPath = path.join(articleMaterialDir, `${articleId}`);
-    const articleRecycleDirPath = path.join(articleMaterialDir, '_recycle', `${articleId}`);
-    if (!await exists(articleDirPath)) {
-        return;
-    }
-    if (await exists(articleRecycleDirPath)) {
-      await unlink(articleRecycleDirPath);
-    }
-    await rename(articleDirPath, articleRecycleDirPath)
-  }
-
-  async restoreArticle() {
-    const { jianghuKnex } = this.app;
-    const actionData = this.ctx.request.body.appData.actionData;
-    validateUtil.validate(actionDataScheme.restoreArticle, actionData);
-    const { articleId } = actionData;
-    const { articleMaterialDir } = this.app.config;
-
-    await jianghuKnex(tableEnum.article, this.ctx).where({ articleId }).update({ articlePublishStatus: articlePublishStatusEnum.login });
-
-    const articleDirPath = path.join(articleMaterialDir, `${articleId}`);
-    const articleRecycleDirPath = path.join(articleMaterialDir, '_recycle', `${articleId}`);
-    if (!await exists(articleRecycleDirPath) || await exists(articleDirPath)) {
-        return;
-    }
-    await rename(articleRecycleDirPath, articleDirPath);
-  }
 
   async getArticleAndFillArticles() {
     const { ctx, app } = this;
