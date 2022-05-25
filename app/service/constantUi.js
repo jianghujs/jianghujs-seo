27b80554e88db47b9e61702df8839bbb405708f4
Liu Ji <mynameisliuji@foxmail.com>
2022-05-25 23:31:27 +0800

    constantUiList

--- app/service/constantUi.js
+++ app/service/constantUi.js
@@ -19,36 +19,9 @@ class ConstantUiService extends Service {
     const { pageId } = this.ctx.packagePage;
     const { language } = this.app.config;
     const constantUiList = await jianghuKnex(tableEnum._constant_ui).whereIn('pageId', ['all', pageId]).select();
-    const constantUi = {};
-
-    for (const item of constantUiList) {
-      const menuArray = ['array', 'object'].includes(item.constantType) ? JSON.parse(item[language]) : item[language];
-      if (item.constantKey === 'header') {
-        const menuArticleIds = _.map(menuArray.menuList, (item, index) => {
-          if (item.path.includes('/page/article/')) {
-            return item.path.split('/')[3];
-          }
-        });
-
-        if (pageId === 'article') {
-          if (!menuArticleIds.length) continue;
-          const articleId = ctx.pathParams && ctx.pathParams[0] || this.ctx.request.body.appData.actionData.articleId;
-          const articleList = await jianghuKnex(tableEnum.article)
-            .whereIn('articleId', menuArticleIds)
-            .select(['categoryId', 'articleId']);
-          const nowArticle = await jianghuKnex(tableEnum.article)
-            .where('articleId', articleId)
-            .first(['categoryId']);
-          const nowArticleCache = articleList.find(e => +e.categoryId === +nowArticle.categoryId);
-          _.forEach(menuArray.menuList, (item, index) => {
-            if (item.path.includes(nowArticleCache.articleId)) {
-              item.active = 'layui-this';
-            }
-          })
-        }
-      }
-      constantUi[item.constantKey] = menuArray;
-    }
+    const constantUi = Object.fromEntries(
+      constantUiList.map(obj => [obj.constantKey, JSON.parse(obj[language] || '{}')])
+    );
     return constantUi;
   }
 }
