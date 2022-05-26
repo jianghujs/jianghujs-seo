549f41ecdae4e6b004e43d25d895c21b13563839
Liu Ji <mynameisliuji@foxmail.com>
2022-05-26 22:01:30 +0800

    "field": "constantUiMap", "service": "constantUi", "serviceFunc": "getConstantUiMap"

--- app/service/constantUi.js
+++ app/service/constantUi.js
@@ -2,19 +2,13 @@
 
 // ========================================常用 require start===========================================
 const Service = require('egg').Service;
-const { BizError } = require('../constant/error');
-const validateUtil = require('egg-jianghu/app/common/validateUtil');
-const idGenerateUtil = require('egg-jianghu/app/common/idGenerateUtil');
 const { tableEnum } = require('../constant/constant');
 // ========================================常用 require end=============================================
-const _ = require('lodash');
-const md5 = require('md5-node');
 
 
 class ConstantUiService extends Service {
 
-  async constantUiList() {
-    const { ctx } = this;
+  async getConstantUiMap() {
     const { jianghuKnex } = this.app;
     const { pageId } = this.ctx.packagePage;
     const { language } = this.app.config;
